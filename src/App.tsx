b2eeb9155822c404298f3c73657f4b412a82a60a
Karu5333 <Karu5333t@yahoo.co.jp>
2025-05-14 21:56:50 +0900

    初回コミット

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TitleBar from './components/TitleBar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './Home';
+import Products from './components/Products';
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TitleBar/>
       </header>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/products" element={<Products/>} />
+
+      </Routes>
     </div>
+    </Router>
   );
 }
 
